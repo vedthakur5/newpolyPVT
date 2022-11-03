5477adf92b1d94df642575642dde7e38b2c75221
Krishna Thoriya <70200639+Krishna0312@users.noreply.github.com>
2022-11-03 12:37:10 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -10,7 +10,7 @@ import cv2
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--testsize', type=int, default=352, help='testing size')
-    parser.add_argument('--pth_path', type=str, default='./model_pth/PolypPVT.pth')
+    parser.add_argument('--pth_path', type=str, default=f'/content/drive/MyDrive/BTP/41PolypPVT.pth')
     opt = parser.parse_args()
     model = PolypPVT()
     model.load_state_dict(torch.load(opt.pth_path))
