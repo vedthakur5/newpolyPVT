30ab68203c4be3131a104e8aed62010af5069a89
Krishna Thoriya <70200639+Krishna0312@users.noreply.github.com>
2022-11-19 03:10:52 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -41,3 +41,13 @@ if __name__ == '__main__':
             res = (res - res.min()) / (res.max() - res.min() + 1e-8)
             cv2.imwrite(save_path+name, res*255)
         print(_data_name, 'Finish!')
+        
+        ############### visualize in tabular manner and show  final dice score for test data   ########################
+        
+#        inputs, masks = next(iter(val_loader))
+#        output = ((torch.sigmoid(model(inputs.to('cuda')))) >0.5).float()
+#        _, ax = plt.subplots(2,3, figsize=(15,10))
+#        for k in range(2):
+#            ax[k][0].imshow(inputs[k].permute(1,2,0))
+#            ax[k][1].imshow(output[k][0].cpu())
+#            ax[k][2].imshow(masks[k])
